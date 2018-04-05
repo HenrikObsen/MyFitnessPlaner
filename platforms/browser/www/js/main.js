f34b2afa85c3f4d3700580d85aa9e7ff36e167b5
Henrik Obsen <hto@e-train.dk>
2018-04-05 17:01:19 +0200

    start

---
+++ platforms/browser/www/js/main.js
@@ -0,0 +1,45 @@
+  // if (window.localStorage.getItem("user") == null) {
+        //     window.location.assign("../index.html");
+        // }
+
+function navigate(loca, id) {
+    //alert(loca + id);
+    if (id > 0) {
+        window.localStorage.setItem("id", id);
+    }
+    window.location.assign(loca + ".html");
+}
+
+$(document).ready(function () {
+    $("#toggleMenu").click(function () {
+        $("#menu-panel").slideToggle("slow");
+        $('html,body').animate({ scrollTop: $("body").offset().top }, 'slow');
+        
+    });
+
+    $(".mobileMenuItem").click(function () {
+        $("#menu-panel").slideUp("slow");
+    });
+
+    $("#navBestilling").click(function () {
+        $("#menuBestilling").slideToggle("slow");
+        $("#menuKontakt").slideUp("slow");
+        
+    });
+
+    $(".bestillingMenuItem").click(function () {
+        $("#menuBestilling").slideUp("slow");
+    });
+
+    $("#navContact").click(function () {
+
+        $("#menuKontakt").slideToggle("slow");
+        $("#menuBestilling").slideUp("slow");
+
+        
+    });
+
+    $(".kontaktMenuItem").click(function () {
+        $("#menuKontakt").slideUp("slow");
+    });
+});
